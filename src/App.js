3571ae478cfbd7194f77562e9b052360821225cf
Or Cherfas <ocherfas@gmail.com>
2023-10-11 20:28:35 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+// Pass in your AWS S3 credentials
+
+
+const s3Client = new S3Client({
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_KEY,
+  },
+});
 
 function App() {
+  const [fileUrl, setFileUrl] = useState(null)
+
+  useEffect(() => {
+    const fetchVideo = async () => {
+      const getObjectParams = {
+        Bucket: BUCKET_NAME,
+        Key: PATH_TO_FILE,
+      };
+
+      const command = new GetObjectCommand(getObjectParams);
+      const response = await s3Client.send(command);
+      const videoDataUrl = URL.createObjectURL(new Blob([response.Body]));
+      setFileUrl(videoDataUrl)
+    }
+
+    fetchVideo()
+  }, [])
+
+  console.log(fileUrl)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {fileUrl && (
+          <video controls width="640" height="360">
+            <source src={fileUrl} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        )}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
